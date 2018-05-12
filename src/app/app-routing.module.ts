4eea3da8198120490b24bc2a6f41e0980c7851bf
Shalitha Suranga <shalithasuranga@gmail.com>
2018-05-12 08:40:13 +0530

    Add game routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartscreenComponent } from './components/startscreen/startscreen.component';
+import { GamescreenComponent } from './components/gamescreen/gamescreen.component';
 
 
 const routes: Routes = [
-  {path : 'start', component : StartscreenComponent}
+  {path : 'start', component : StartscreenComponent},
+  {path : 'game', component : GamescreenComponent}
 ];
 
 @NgModule({
