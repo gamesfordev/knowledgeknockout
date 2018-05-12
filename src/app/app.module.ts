c459311edb027a7c07beae0836775d302422106f
Asce4s <asce4s@gmail.com>
2018-05-12 08:03:18 +0530

    init setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TerminalComponent } from './components/terminal/terminal.component';
+import { ResultComponent } from './components/result/result.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TerminalComponent,
+    ResultComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule
