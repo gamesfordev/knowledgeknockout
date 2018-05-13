0b19375072541655975bf4b0adb353d644bd0599
Shalitha Suranga <shalithasuranga@gmail.com>
2018-05-13 16:26:32 +0530

    Twitter tweet

--- src/app/components/leaderboardscreen/leaderboardscreen.component.ts
+++ src/app/components/leaderboardscreen/leaderboardscreen.component.ts
@@ -48,7 +48,10 @@ export class LeaderboardscreenComponent implements OnInit {
   }
 
   tweetNow(): void {
-    window.open('https://twitter.com/intent/tweet?text=Knowledge Knockout - Play terminal command challenge game and be a MasterGeek ' + window.location.origin , '_blank');
+    if(this.score >= 100)
+      window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('I got MasterGeek badge on #KnowledgeKnockout ' + window.location.origin) , '_blank');
+    else
+      window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('My MasterGeek level is  ' + this.score + '%25 on #KnowledgeKnockout ' + window.location.origin) , '_blank');
   }
 
 }
