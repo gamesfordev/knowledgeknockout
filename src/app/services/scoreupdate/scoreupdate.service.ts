c1bbae3a2d1d0ec5cb3e5931081d72ba108577e5
Shalitha Suranga <shalithasuranga@gmail.com>
2018-05-13 22:58:24 +0530

    Change times

--- src/app/services/scoreupdate/scoreupdate.service.ts
+++ src/app/services/scoreupdate/scoreupdate.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class ScoreupdateService {
 
   public score: number = 0;
-  public MAX_SCORE: number = 10;
+  public MAX_SCORE: number = 30;
 
   constructor() { }
 }
