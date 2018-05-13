64ae3f01f7f25d18e79943b4a3a7413719a61ef4
Shalitha Suranga <shalithasuranga@gmail.com>
2018-05-13 07:13:27 +0530

    Add express server for deployment

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(__dirname + '/dist/angularattack2018-nullpointer'));
+
+/*const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+
+app.use(forceSSL());
+*/
+
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+
+
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/angularattack2018-nullpointer/index.html'));
+});
+
+let PORT = process.env.PORT || 8080;
+
+let listener = app.listen(PORT , () => {
+    console.log('server is running at ' + PORT);
+});
